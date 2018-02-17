f97b47e7462337a34229c73de3fdb4ae25e26e05
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2018-02-17 10:10:40 -0500

    Replace nm-applet with wpa_gui

--- scripts/.local/bin/rofi_logout.sh
+++ scripts/.local/bin/rofi_logout.sh
@@ -2,3 +2,14 @@
 
 # logout script for use with rofi
 # options: cancel, logout, shut down, restart, hibernate, suspend, lock
+
+OPTIONS="\
+  logout\
+  \nshutdown\
+  \nrestart\
+  \nhibernate\
+  \nsuspend\
+  \nlock\
+"
+
+echo $OPTIONS | rofi -i -dmenu
