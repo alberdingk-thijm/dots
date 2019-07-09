7319719d4699c97678924581abc764eb54748cb1
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2019-07-09 09:33:48 -0400

    [scripts] update rofi_logout.sh

--- scripts/.local/bin/rofi_logout.sh
+++ scripts/.local/bin/rofi_logout.sh
@@ -2,3 +2,33 @@
 
 # logout script for use with rofi
 # options: cancel, logout, shut down, restart, hibernate, suspend, lock
+
+OPTIONS="cancel|logout|shutdown|restart|hibernate|suspend|lock"
+
+choice=$(echo "$OPTIONS" | rofi -sep '|' -i -dmenu -p "Exit: ")
+
+case $choice in
+    "cancel") ;;
+    "logout")
+        # execute logout
+        ;;
+    "shutdown")
+        # execute shutdown
+        sudo shutdown -h now
+        ;;
+    "restart")
+        # execute restart
+        sudo reboot
+        ;;
+    "hibernate")
+        # execute hibernate
+        ;;
+    "suspend")
+        # execute suspend
+        sudo zzz
+        ;;
+    "lock")
+        # execute lock
+        dm-tool lock
+        ;;
+esac
